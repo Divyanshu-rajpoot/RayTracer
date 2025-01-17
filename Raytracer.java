16a1dc8dbd85b8cd46b957cc3aa758253ee21a44
Divyanshu-rajpoot <divy.r.007@gmail.com>
2025-01-17 14:34:41 +0530

    vector class added

--- Raytracer.java
+++ Raytracer.java
@@ -5,7 +5,7 @@ public class Raytracer {
     static int Image_width = 256;
     static int Image_height = 256;
     public static void main(String[] args) {
-        
+
 
         try {
             FileWriter writer = new FileWriter("render.ppm");
