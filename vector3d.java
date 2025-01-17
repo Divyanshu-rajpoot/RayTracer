c0030c8ca018111752c8296edf9a3e0c730b8c49
Divyanshu-rajpoot <divy.r.007@gmail.com>
2025-01-17 14:29:57 +0530

    vector class added

---
+++ vector3d.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class vector3d {
+
+    private double[] e;  //initializeing the e a vector value
+
+    public vector3d(){   //contructing a vextor / or initilaizing the vector
+        e = new double[]{0,0,0};   //initialze the vector positions
+    }
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
