5dc2594d94c1fea179c7088c22d674d6d653f8ee
Divyanshu-rajpoot <divy.r.007@gmail.com>
2025-01-20 16:01:00 +0530

    some vector classes added

--- Raytracer.java
+++ Raytracer.java
@@ -4,24 +4,24 @@ public class Raytracer {
 
     static int Image_width = 256;
     static int Image_height = 256;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
         try {
             FileWriter writer = new FileWriter("render.ppm");
 
-            //writing the general format of ppm image
+            // writing the general format of ppm image
             writer.write("P3\n");
             writer.write(Image_height + " " + Image_width + "\n");
             writer.write("255\n");
 
-            //creating a pattern
+            // creating a pattern
             for (int i = 0; i < Image_height; i++) {
-                System.out.print("\rLines Remaining " + (Image_height-i));
+                System.out.print("\rLines Remaining " + (Image_height - i));
                 // System.out.flush();
                 for (int j = 0; j < Image_width; j++) {
-                    double r = (double) j / (Image_width - 1) ;
-                    double g = (double) i / (Image_height - 1) ; 
+                    double r = (double) j / (Image_width - 1);
+                    double g = (double) i / (Image_height - 1);
                     double b = 0.0;
 
                     int ir = (int) (255.999 * r);
@@ -30,12 +30,12 @@ public class Raytracer {
 
                     writer.write(ir + " " + ig + " " + ib + "\n");
                 }
-                
+
             }
             System.out.print("\rDone               \n");
             writer.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }        
+        }
     }
 }
--- vector3d.java
+++ vector3d.java
@@ -36,9 +36,66 @@ public class vector3d {
     }
 
     public vector3d add (vector3d v){         // addition of vector 3d classes
-        
+        return new vector3d(e[0] + v.e[0], e[0]+ v.e[1], e[0]+v.e[2]);   //return a new vector added
     }
 
+    public vector3d multiple (double t){     //multiply the vector with a t co-ordinate
+        return new vector3d(e[0] * t, t * e[1], t*e[2]);
+    }
+
+    public vector3d divide(double scalar){   //divide the vector with a scalar quantity
+        return multiple(1/scalar);
+    }
+
+    public double length(){
+        return Math.sqrt(square_length());
+    }
+
+    public double square_length(){           //squared the length of vector co-ordinate
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+    //starting of the utility functions
+
+
+    //display the vector in strings
+    public static String vectoString(vector3d vec){
+        return vec.x() + " " + vec.y() + " " + vec.z();
+    }
+
+    public static vector3d add(vector3d vec1 , vector3d vec2){ //addition forntion for two vectors
+        return new vector3d(vec1.e[0] + vec2.e[0],vec1.e[1] + vec2.e[1] , vec1.e[2] + vec2.e[2] );
+    }
+
+    public static vector3d subtract(vector3d vec1 , vector3d vec2){
+        return new vector3d(vec1.e[0] - vec2.e[0], vec1.e[1] - vec2.e[1], vec1.e[2] - vec2.e[2]);
+    }
+
+    public static vector3d multiply(vector3d vec1 , vector3d vec2){
+        return new vector3d(vec1.e[0] * vec2.e[0], vec1.e[1] * vec2.e[1], vec1.e[2] * vec2.e[2]);
+    }
+
+
+
+
+
+
+
+    //using point 3 vector   //use the poitn3 as a vector now
+    class Point3 extends vector3d{
+        public Point3(){
+            super();
+        }
+        public Point3(double e0 , double e1 , double e2){
+            super(e0, e1 , e2);
+        }
+    }
+
+
+
+
+
+    
 
     public static void main(String[] args) {
         
